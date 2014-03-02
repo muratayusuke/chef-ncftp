4bb94f3997b61545bb0e9b04766d1903fd32dd5c
muratayusuke <info@muratayusuke.com>
2014-03-02 09:30:04 +0900

    fix url

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ execute "Extracting and Building ncftp #{node['ncftp']['version']} from Source" 
   cwd Chef::Config['file_cache_path']
   command <<-COMMAND
     export http_proxy=#{node['proxy']}
-    wget ftp://ftp.ncftp.com/ncftp/ncftp-#{node['ncftp']['version']}-src.tar.gz
+    wget #{node['ncftp']['url']}
     tar xzf ncftp-#{node['ncftp']['version']}-src.tar.gz
     cd ncftp-#{node['ncftp']['version']}
     ./configure --prefix=#{node['ncftp']['prefix']}
